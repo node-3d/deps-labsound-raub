83cb22d2545ced971643b04b1963d24b54fa94b3
Luis Blanco <luisblanco1337@gmail.com>
2020-06-14 23:17:04 +0300

    :hammer: Rebuild

--- src/unpack.sh
+++ src/unpack.sh
@@ -5,7 +5,7 @@ echo 'Unpack Started'
 	cd src
 	
 	unzip -qq LabSound-master.zip -d .
-	# unzip -qq alsa.zip -d LabSound-master/include
+	unzip -qq alsa.zip -d LabSound-master/include
 	
 	unzip -qq libnyquist-master.zip -d LabSound-master/third_party/libnyquist
 	mv LabSound-master/third_party/libnyquist/libnyquist-master/* LabSound-master/third_party/libnyquist
