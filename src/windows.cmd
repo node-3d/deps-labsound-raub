7cf947980bd6cfccbc883b587a91c1613c998744
Luis Blanco <luisblanco1337@gmail.com>
2021-05-11 23:30:45 +0300

    Fix windows build

--- src/windows.cmd
+++ src/windows.cmd
@@ -5,6 +5,10 @@ cd src
 rd /s /q "LabSound-1.0.1"
 tar -xf LabSound-1.0.1.zip
 
+tar -xf alsa.zip -C LabSound-1.0.1/include
+tar -xf libnyquist-master.zip -C LabSound-1.0.1/third_party/libnyquist
+xcopy "LabSound-1.0.1/third_party/libnyquist/libnyquist-master" "LabSound-1.0.1/third_party/libnyquist" /i /c /k /e /r /y
+
 rd /s /q "build"
 mkdir build
 
@@ -26,6 +30,8 @@ dir
 cd ../../../..
 
 copy /y LabSound-1.0.1\build\bin\Release\LabSound.lib build\LabSound.lib
+copy /y LabSound-1.0.1\build\bin\Release\libnyquist.lib build\libnyquist.lib
+copy /y LabSound-1.0.1\build\bin\Release\libwavpack.lib build\libwavpack.lib
 
 cd ..
 
