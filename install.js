2c563777bb1bd85798525c0cd86a904fd4516e08
Luis Blanco <luisblanco1337@gmail.com>
2023-11-17 14:19:40 +0400

    Fix package lock

--- install.js
+++ install.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const install = require('addon-tools-raub/install');
+const { install } = require('addon-tools-raub');
 
 
 const prefix = 'https://github.com/node-3d/deps-labsound-raub/releases/download';
