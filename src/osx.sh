4890f603a5f22ef7784438275817417256ff21f8
Luis Blanco <luisblanco1337@gmail.com>
2023-11-17 23:36:51 +0400

    Fix osx paths

--- src/osx.sh
+++ src/osx.sh
@@ -10,9 +10,6 @@
 	cmake --build . --target LabSound --config Release
 )
 
-cp src/LabSound-1.2.0/build/bin/LabSound.framework/Versions/A/LabSound src/build/LabSound
-cp src/LabSound-1.2.0/build/third_party/libnyquist/lib/liblibnyquist.a src/build/liblibnyquist.a
-cp src/LabSound-1.2.0/build/third_party/libsamplerate/src/libsamplerate.a src/build/libsamplerate.a
-
-ls src/LabSound-1.2.0/build/bin
-ls src/LabSound-1.2.0/build/bin/Release
+cp src/LabSound-1.2.0/build/bin/Release/LabSound.framework/Versions/A/LabSound src/build/LabSound
+cp src/LabSound-1.2.0/build/third_party/libnyquist/lib/Release/liblibnyquist.a src/build/liblibnyquist.a
+cp src/LabSound-1.2.0/build/third_party/libsamplerate/src/Release/libsamplerate.a src/build/libsamplerate.a
