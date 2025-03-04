a409e37301c41561a086db2086dcd1b4ad1ef036
Luis Blanco <luisblanco1337@gmail.com>
2025-03-04 20:41:29 +0400

    Use internal labsound repo

--- src/extract.sh
+++ src/extract.sh
@@ -1,17 +1,17 @@
 (
 	cd src
 	
-	rm -rf build LabSound-1.2.0
+	rm -rf build LabSound
 	mkdir -p build
 	
-	git clone --depth 1 -b Bugfixes https://github.com/Avataren/LabSound.git LabSound-1.2.0
+	git clone --depth 1 -b test-1.3.0 https://github.com/raub/LabSound.git LabSound
 	
 	rm -rf ../include/LabSound
 	mkdir -p ../include/LabSound
-	cp -r LabSound-1.2.0/include/LabSound/* ../include/LabSound
+	cp -r LabSound/include/LabSound/* ../include/LabSound
 	
 	(
-		cd LabSound-1.2.0/third_party
+		cd LabSound/third_party
 		git clone --depth 1 -b 0.2.2 https://github.com/libsndfile/libsamplerate.git
 		git clone --depth 1 -b 0.11.22 https://github.com/mackron/miniaudio.git
 		git clone --depth 1 -b v0.7 https://github.com/dafx/libnyquist.git
