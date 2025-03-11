948b0e4d5ed12089f0a057e312b13437297c087c
Luis Blanco <luisblanco1337@gmail.com>
2025-03-11 21:01:24 +0400

    Try simplified build

--- src/linux.sh
+++ src/linux.sh
@@ -1,3 +1,3 @@
 chmod +x src/linux-common.sh && src/linux-common.sh
 
-cp src/LabSound/build/third_party/libnyquist/lib/liblibnyquist.a src/build/liblibnyquist.a
+# cp src/LabSound/build/third_party/libnyquist/lib/liblibnyquist.a src/build/liblibnyquist.a
--- src/windows.sh
+++ src/windows.sh
@@ -6,15 +6,15 @@
 	cmake -A x64 -T v142 ..
 	cmake --build . --target libnyquist --config Release
 	cmake --build . --target LabSound --config Release
-	cmake --build . --target LabSoundRtAudio --config Release
-	cmake --build . --target LabSoundMiniAudio --config Release
+	# cmake --build . --target LabSoundRtAudio --config Release
+	# cmake --build . --target LabSoundMiniAudio --config Release
 )
 
 (
 	cd src
 	
 	cp LabSound/build/bin/Release/LabSound.lib build/LabSound.lib
-	cp LabSound/build/bin/Release/LabSoundRtAudio.lib build/LabSoundRtAudio.lib
-	cp LabSound/build/bin/Release/LabSoundMiniAudio.lib build/LabSoundMiniAudio.lib
+	# cp LabSound/build/bin/Release/LabSoundRtAudio.lib build/LabSoundRtAudio.lib
+	# cp LabSound/build/bin/Release/LabSoundMiniAudio.lib build/LabSoundMiniAudio.lib
 	cp LabSound/build/third_party/libnyquist/lib/Release/libnyquist.lib build/libnyquist.lib
 )
