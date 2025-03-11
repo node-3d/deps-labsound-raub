f692a5969144d72275a61fd08bbf60864938b23f
Luis Blanco <luisblanco1337@gmail.com>
2025-03-11 21:04:41 +0400

    Fix cmake deps

--- src/extract.sh
+++ src/extract.sh
@@ -13,7 +13,7 @@
 	(
 		cd LabSound/third_party
 		git clone --depth 1 -b 0.2.2 https://github.com/libsndfile/libsamplerate.git
-		# git clone --depth 1 -b 0.11.22 https://github.com/mackron/miniaudio.git
+		git clone --depth 1 -b 0.11.22 https://github.com/mackron/miniaudio.git
 		git clone --depth 1 https://github.com/ddiakopoulos/libnyquist.git
 	)
 )
